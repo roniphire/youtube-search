0a89ddda373b7cd2dc2c34dc8a8cb6f22f09b82a
roniphire <roni@phirestudio.com>
2020-08-10 01:09:40 +0700

    search bar

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <SearchBar @searchChange="onSearchChange"></SearchBar>
+  </div>
+</template>
+
+<script>
+import SearchBar from "./components/SearchBar.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    SearchBar,
+  },
+
+  methods: {
+    onSearchChange(searchTerm) {
+      console.info(searchTerm);
+    },
+  },
+};
+</script>
+
+
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input @input="onInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+
+  methods: {
+    onInput: function (e) {
+      this.$emit("searchChange", e.target.value);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
